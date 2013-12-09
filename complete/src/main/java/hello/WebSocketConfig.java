4db466e4ccab67d1a9f4f0551868266a6bb1881a
Greg Turnquist <gturnquist@gopivotal.com>
2013-12-09 14:51:35 -0600

    Resolves #3: Upgrade to Tomcat 7.0.47
    By using Spring Boot BUILD-SNAPSHOT, was able to get this working properly and use
    the correct APIs. Will open parallel issue to migrate back to an official
    release instead of simply using snapshots to stabilize after one comes out
    with this version of Spring/Tomcat.

--- complete/src/main/java/hello/WebSocketConfig.java
+++ complete/src/main/java/hello/WebSocketConfig.java
@@ -1,24 +1,34 @@
+
 package hello;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.EnableWebSocketMessageBroker;
-import org.springframework.messaging.simp.config.MessageBrokerConfigurer;
-import org.springframework.messaging.simp.config.StompEndpointRegistry;
-import org.springframework.messaging.simp.config.WebSocketMessageBrokerConfigurer;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
-	public void configureMessageBroker(MessageBrokerConfigurer config) {
+	public void configureMessageBroker(MessageBrokerRegistry config) {
 		config.enableSimpleBroker("/queue/");
-		config.setAnnotationMethodDestinationPrefixes("/app");
+        config.setApplicationDestinationPrefixes("/app");
 	}
-	
+
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/hello").withSockJS();
 	}
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration channelRegistration) {
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration channelRegistration) {
+    }
 
 }
