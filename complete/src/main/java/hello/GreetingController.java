e01fda506e2a13804ef6fb6a0c2630b01285fb56
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2013-12-12 12:52:01 -0500

    Simplify greeting method
    Replace messaging template with @SendTo.

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,27 +1,19 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
 public class GreetingController {
 
-    private SimpMessageSendingOperations messagingTemplate;
 
-    @Autowired
-    public GreetingController(SimpMessageSendingOperations messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
-    }
-    
     @MessageMapping("/hello")
-    public void greeting(HelloMessage message) throws Exception {
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
         Thread.sleep(3000); // simulated delay
-        Greeting greeting = new Greeting("Hello, " + message.getName() + "!");
-        messagingTemplate.convertAndSend("/queue/greetings", greeting);
+        return new Greeting("Hello, " + message.getName() + "!");
     }
 
 }
