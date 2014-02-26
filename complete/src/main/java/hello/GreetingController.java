be1e8651ae70730dfdcd894d96c35f316921c9ce
Sebastien Deleuze <sdeleuze@gopivotal.com>
2014-02-26 11:45:43 +0100

    Use AbstractWebSocketMessageBrokerConfigurer
    Fix the build by extending AbstractWebSocketMessageBrokerConfigurer
    in order to provide unimplemented methods.
    Also upgrade Gradle build dependencies to Spring
    Boot 1.0.0.RC3.

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -4,7 +4,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-
 @Controller
 public class GreetingController {
 
--- complete/src/main/java/hello/WebSocketConfig.java
+++ complete/src/main/java/hello/WebSocketConfig.java
@@ -1,16 +1,14 @@
-
 package hello;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -21,14 +19,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/hello").withSockJS();
-	}
-
-	@Override
-	public void configureClientInboundChannel(ChannelRegistration channelRegistration) {
-	}
-
-	@Override
-	public void configureClientOutboundChannel(ChannelRegistration channelRegistration) {
 	}
 
 }
