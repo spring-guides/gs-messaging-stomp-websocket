5f5ce6f985109ef06f62309b5eba1c8ce447b63e
Brian Clozel <bclozel@pivotal.io>
2016-08-30 23:20:21 +0200

    Add integration test
    This commit adds an integration test that uses the
    `WebSocketStompClient` and `SockJsClient` to send STOMP messages to the
    server and check for a response message with the dedicated topic.
    
    Also a few changes to refresh the guide:
    * disambiguate paths in message mappings, sockjs configuration, topics
    and fix guide wrt SockJS server configuration
    * use webjars and refresh front-end
    * separate client app in a dedicated file and use jquery
    * clean dependencies

--- complete/src/main/java/hello/HelloMessage.java
+++ complete/src/main/java/hello/HelloMessage.java
@@ -3,9 +3,19 @@ package hello;
 public class HelloMessage {
 
     private String name;
-    
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- complete/src/main/java/hello/WebSocketConfig.java
+++ complete/src/main/java/hello/WebSocketConfig.java
@@ -10,15 +10,15 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic");
-		config.setApplicationDestinationPrefixes("/app");
-	}
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
 
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/hello").withSockJS();
-	}
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
 
 }
