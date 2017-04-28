75b904e9149eb6260f693ba14e2536e614c9ea2d
Juaniyyoo <john.guignard@gmail.com>
2017-04-28 23:38:09 +0200

    Add chat_messages WebSocket broker

--- complete/src/main/java/hello/WebSocketConfig.java
+++ complete/src/main/java/hello/WebSocketConfig.java
@@ -13,6 +13,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/chat_messages");
         config.setApplicationDestinationPrefixes("/app");
     }
 
