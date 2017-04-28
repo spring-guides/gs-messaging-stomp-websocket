4bcd4e961febf211aa0d8cdda7d7663bb4842410
Juaniyyoo <john.guignard@gmail.com>
2017-04-28 23:36:56 +0200

    Remove simulation delay

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -11,7 +11,6 @@ public class GreetingController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(1000); // simulated delay
         return new Greeting("Hello, " + message.getName() + "!");
     }
 
