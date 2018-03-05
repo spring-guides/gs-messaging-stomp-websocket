234ca3ad9c8653226445f3588c06603a27e0d94d
Brian Clozel <bclozel@pivotal.io>
2018-03-05 21:32:51 +0100

    Upgrade to Spring Boot 2.0.0.RELEASE

--- complete/src/test/java/hello/GreetingIntegrationTests.java
+++ complete/src/test/java/hello/GreetingIntegrationTests.java
@@ -13,8 +13,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
