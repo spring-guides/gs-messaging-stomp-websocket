469c04861759dc3fdfdca841861e9ad08fdadd04
greenyleaf <sdrkyj@163.com>
2025-07-26 20:40:26 +0800

    Upgrade to Spring Boot 3.5.4; format code indent; switch to webjars
    Signed-off-by: greenyleaf <sdrkyj@163.com>

--- initial/src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
+++ initial/src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MessagingStompWebsocketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingStompWebsocketApplication.class, args);
+    }
 
 }
